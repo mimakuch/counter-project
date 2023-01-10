ac939f32a05cca55a13e798ccdee58ce96783589
Michal Makuch <michalmakuch@hotmail.com>
2023-01-10 23:32:39 +0100

    added render and init

---
+++ Counter.js
@@ -0,0 +1,36 @@
+
+
+const Counter = function (selector) {
+    const container = document.querySelector(selector)
+
+    if (!container) throw new Error('Container element not found')
+
+    this.selector = selector
+    this.container = container
+    this.number = 0
+}
+
+Counter.prototype.init = function () {
+    this.render()
+}
+
+Counter.prototype.render = function () {
+
+    this.container.innerText = ''
+
+    const h1 = document.createElement('h1')
+    const button = document.createElement('button')
+
+    h1.innerHTML = this.number
+
+    this.container.appendChild(h1)
+}
+
+Counter.prototype.inc = function () {
+    this.number = this.number + 1
+}
+
+Counter.prototype.status = function () {
+    console.log('Current number is: ' + this.number)
+    return this.number
+}
