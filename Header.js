92f7bb56a2f8b7661cd649456004ed85888645ef
Michal Makuch <michalmakuch@hotmail.com>
2023-01-25 00:58:53 +0100

    Header refactored into class

--- Header.js
+++ Header.js
@@ -1,16 +1,20 @@
-const Header = function (text) {
-    this.text = text
-}
+const Header = class {
 
-Header.prototype.render = function () {
-    const h1 = document.createElement('h1')
+    constructor(text) {
+        this.text = text
+    }
 
-    h1.innerText = this.text
+    render() {
+        const h1 = document.createElement('h1')
 
-    h1.style.padding = '4px'
-    h1.style.margin = '4px'
-    h1.style.minWidth = '20px'
-    h1.style.fontFamily = 'sans-serif'
+        h1.innerText = this.text
 
-    return h1
+        h1.style.padding = '4px'
+        h1.style.margin = '4px'
+        h1.style.minWidth = '20px'
+        h1.style.fontFamily = 'sans-serif'
+
+        return h1
+    }
+
 }
