c518c3e0c0dd57028a35a94e5ea00ed57521c71b
Michal Makuch <michalmakuch@hotmail.com>
2023-01-25 01:04:28 +0100

    Button refactored into class

--- Button.js
+++ Button.js
@@ -1,27 +1,28 @@
-const Button = function (label, onClick) {
-    this.label = label
-    this.onClick = onClick
+const Button = class {
+    constructor(label, onClick) {
+        this.label = label
+        this.onClick = onClick
+    }
+
+    render() {
+        const button = document.createElement('button')
+
+        button.innerText = this.label
+
+        button.style.border = '1px solid rgba(0, 0, 0, 0.1'
+        button.style.outline = 'none'
+        button.style.borderRadius = '4px'
+        button.style.backgroundColor = '#ffffff'
+        button.style.padding = '4px'
+        button.style.margin = '4px'
+        button.style.minWidth = '20px'
+        button.style.cursor = 'pointer'
+
+        button.addEventListener(
+            'click',
+            () => this.onClick()
+        )
+
+        return button
+    }
 }
-
-Button.prototype.render = function () {
-    const button = document.createElement('button')
-
-    button.innerText = this.label
-
-    button.style.border = '1px solid rgba(0, 0, 0, 0.1'
-    button.style.outline = 'none'
-    button.style.borderRadius = '4px'
-    button.style.backgroundColor = '#ffffff'
-    button.style.padding = '4px'
-    button.style.margin = '4px'
-    button.style.minWidth = '20px'
-    button.style.cursor = 'pointer'
-
-    button.addEventListener(
-        'click',
-        () => this.onClick()
-    )
-
-    return button
-}
-
